a82fb4a1bc8b966fbaec80ba3cbe41e0e706d59b
Magpie <edchavez821@gmail.com>
2019-05-03 13:56:23 -0500

    responsive version and sound design

--- scripts/addroutine.js
+++ scripts/addroutine.js
@@ -26,17 +26,20 @@ $(".time-display").hide();
 var database = firebase.database();
 var rows = 0;
 var cycles = 1;
+var schedule = [];
 var currentCount = 0;
 var uRoutinesArr = [];
+var cycleTime;
+var totalTime;
 
 function calculateTotal() {
-  var cycleTime = 0;
+  cycleTime = 0;
   $(".new-exercise").each(function(){
     cycleTime += parseInt($(this).attr("time"));
   });
-  var totalTime = cycleTime * cycles;
-  $(".cycle-time").text("Cycle duration: " + cycleTime);
-  $(".total-time").text("Total duration: " + totalTime);
+  totalTime = cycleTime * cycles;
+  $(".cycle-time").text("Cycle: " + timeConverter(cycleTime));
+  $(".total-time").text("Total: " + timeConverter(totalTime));
 };
 
 $("#add-exercise").on("click", function () {
@@ -49,7 +52,7 @@ $("#add-exercise").on("click", function () {
   newExercise.addClass("new-exercise row mt-1");
   newExercise.attr("time", "30");
   newExercise.attr("row", rows);
-  newExercise.find(".label").text(name);
+  newExercise.find(".name-label").text(name);
   $(".display-routine").append(newExercise);
   $(".time-display").show();
   calculateTotal();
@@ -63,10 +66,24 @@ $("#add-break").on("click", function () {
   newBreak.addClass("new-exercise row mt-1");
   newBreak.attr("time", "30");
   newBreak.attr("row", rows);
-  newBreak.find(".label").text("Break");
+  newBreak.find(".name-label").text("Break");
   $(".display-routine").append(newBreak);
   $(".time-display").show();
   calculateTotal();
+});
+
+$(document).on("change keyup", ".seconds", function() {
+  var inputTime = $(this).val();
+  $(this).closest(".new-exercise").attr("time", inputTime);
+  calculateTotal();
+  console.log("change");
+});
+
+$(document).on("click", "#remove-exercise", function () {
+  var parentExercise = $(this).closest(".new-exercise");
+  parentExercise.remove();
+  calculateTotal();
+  console.log("removed!");
 });
 
 $(document).on("click", "#add-time", function () {
@@ -75,7 +92,7 @@ $(document).on("click", "#add-time", function () {
   var newTime = timeAttr + 10;
   thisRow.attr("time", newTime)
   var displayTime = thisRow.find(".seconds");
-  displayTime.val(newTime + " sec");
+  displayTime.val(newTime);
   calculateTotal();
 });
 
@@ -85,7 +102,7 @@ $(document).on("click", "#subtract-time", function () {
   var newTime = timeAttr - 10;
   thisRow.attr("time", newTime)
   var displayTime = thisRow.find(".seconds");
-  displayTime.val(newTime + " sec");
+  displayTime.val(newTime);
   calculateTotal();
 });
 
@@ -101,6 +118,17 @@ $("#remove-cycle").on("click", function () {
   calculateTotal();
 });
 
+$("#save-days").on("click", function () {
+  event.preventDefault();
+  schedule.length = 0;
+  $.each($("input[id='dropdownCheck']:checked"), function(){ 
+    var selectedDay = $(this).val();      
+    schedule.push(selectedDay);
+  });
+  $(this).parents(".dropdown").find("button.dropdown-toggle").dropdown("toggle")
+  console.log(schedule);
+});
+
 database.ref().on("value", function(snapshot) {
   if (snapshot.child("routineCount").exists()) {
     currentCount = parseInt(snapshot.val().routineCount);
@@ -114,9 +142,9 @@ $("#save-routine").on("click", function () {
   var routineArray = [];
   $(".new-exercise").each(function(){
     var exerciseObj = {
-      name: $(this).find(".label").text(),
+      name: $(this).find(".name-label").text(),
       number: $(this).attr("row"),
-      length: $(this).attr("time"),
+      length: $(this).find(".seconds").val(),
     };
     routineArray.push(exerciseObj);
   });
@@ -128,8 +156,10 @@ $("#save-routine").on("click", function () {
   newRoutine = routinesRef.child(routineId);
   newRoutine.set({
     name: $("#routine-input").val().trim(),
-    goal: $("#goal-input").val().trim(),
-    target: $("#target-input").val().trim(),
+    type: $("#type-input").val(),
+    target: $("#target-input").val(),
+    days: schedule,
+    duration: totalTime,
     cycles: cycles,
     exercises: routineArray,
   });
@@ -145,24 +175,66 @@ $("#save-routine").on("click", function () {
     else {
       database.ref("/Users/"+firebase.auth().currentUser.uid).child("userRoutines");
     }
-
     console.log(uRoutinesArr)
     uRoutinesArr.push(routineId)
-
-
-    // database.ref("/Users/"+firebase.auth().currentUser.uid).set({
-    //   routines : UroutinsArr
-    // })
     database.ref("/Users/"+firebase.auth().currentUser.uid).update({userRoutines : JSON.stringify(uRoutinesArr)});
   });
-
+  resetRoutine();
+  $(".saved-alert").slideToggle();
 });
 
 
 $("#discard").on("click", function () {
-
+  resetRoutine();
 });
 
+$(".saved-alert").on("click", function () {
+  $(".saved-alert").slideToggle();
+});
+
+function resetRoutine(){
+  $("#routine-input").val("");
+  $("#type-input option:first").attr("selected",true);
+  $("#target-input option:first").attr("selected",true);
+  $("input[id='dropdownCheck']").prop("checked", false);
+  $(".cycles").val("1");
+  $(".display-routine").empty();
+  $(".time-display").hide();
+  $("#name-input").val("");
+  cycles = 1;
+  rows = 0;
+}
+
+function timeConverter(t) {
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return minutes + ":" + seconds;
+};
+
+$(document.body).on("click", "#log-out", function() {
+
+  console.log("log out")
+  firebase.auth().signOut()
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // ...
+    
+  //   alert(errorCode +"message :"+ errorMessage);
+  // });
+
+  location.href = "index.html"
+});
 
 
 
